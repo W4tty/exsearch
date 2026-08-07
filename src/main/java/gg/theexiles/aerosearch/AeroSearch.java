4893cf2f9c37613c50f6e072428a4c80300571a3
Watty <135977262+W4tty@users.noreply.github.com>
2026-08-07 14:49:23 -0400

    Add Aero Search entrypoint

---
+++ src/main/java/gg/theexiles/aerosearch/AeroSearch.java
@@ -0,0 +1,26 @@
+// ? Project: Aero Search
+// ? File: AeroSearch.java
+// ? Directory: /src/main/java/gg/theexiles/aerosearch
+// ? Description: NeoForge entrypoint and client bootstrap.
+// ? Created by: Watty
+// ? Created on: 2026-08-07 14:16 EDT
+// ? Last modified by: Watty
+// ? Last modified on: 2026-08-07 14:16 EDT
+
+package gg.theexiles.aerosearch;
+
+import gg.theexiles.aerosearch.client.AeroSearchClient;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(AeroSearch.MOD_ID)
+public final class AeroSearch {
+    public static final String MOD_ID = "aerosearch";
+    public static final Logger LOGGER = LoggerFactory.getLogger("AeroSearch");
+
+    public AeroSearch() {
+        // ! Client-only mod. All Minecraft client references are isolated in the client package.
+        AeroSearchClient.bootstrap();
+    }
+}
