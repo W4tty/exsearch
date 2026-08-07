8c449082cab14a17cb37eb951b842adfe781ad21
Watty <135977262+W4tty@users.noreply.github.com>
2026-08-07 16:29:57 -0400

    Update runtime mod id and logger to ExSearch

--- src/main/java/gg/theexiles/aerosearch/AeroSearch.java
+++ src/main/java/gg/theexiles/aerosearch/AeroSearch.java
@@ -1,11 +1,11 @@
-// ? Project: Aero Search
+// ? Project: ExSearch
 // ? File: AeroSearch.java
 // ? Directory: /src/main/java/gg/theexiles/aerosearch
 // ? Description: NeoForge entrypoint and client bootstrap.
 // ? Created by: Watty
 // ? Created on: 2026-08-07 14:16 EDT
 // ? Last modified by: Watty
-// ? Last modified on: 2026-08-07 14:16 EDT
+// ? Last modified on: 2026-08-07 16:27 EDT
 
 package gg.theexiles.aerosearch;
 
@@ -16,8 +16,8 @@ import org.slf4j.LoggerFactory;
 
 @Mod(AeroSearch.MOD_ID)
 public final class AeroSearch {
-    public static final String MOD_ID = "aerosearch";
-    public static final Logger LOGGER = LoggerFactory.getLogger("AeroSearch");
+    public static final String MOD_ID = "exsearch";
+    public static final Logger LOGGER = LoggerFactory.getLogger("ExSearch");
 
     public AeroSearch() {
         // ! Client-only mod. All Minecraft client references are isolated in the client package.
